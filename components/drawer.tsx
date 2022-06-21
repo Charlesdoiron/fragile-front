8ca169c995253e30d0e3bd0b68f6232dbd08a91b
charlesdoiron <doironcharles@gmail.com>
2022-06-21 20:37:17 +0200

    drawer

---
+++ components/drawer.tsx
@@ -0,0 +1,75 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
+
+export const Drawer = ({
+  isOpen,
+  handleDrawer,
+}: {
+  isOpen: boolean;
+  handleDrawer: () => void;
+}) => {
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10 font-fragile"
+        onClose={handleDrawer}
+      >
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-md ">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-black py-6 shadow-xl">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex items-start justify-end">
+                        <div className="ml-3 flex h-7 items-center">
+                          <button type="button" onClick={handleDrawer}>
+                            <span className="underline text-white">
+                              No thanks !
+                            </span>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="relative flex-1 px-4 sm:px-6 text-white  text-left mt-20 ml-10">
+                      <h1 className="text-[44px] leading-[44px] mb-16">
+                        Feel free to contact us{" "}
+                      </h1>
+
+                      <h2 className="text-[12px] uppercase mb-2">
+                        FOR COLLABORATION
+                      </h2>
+                      <h3 className="text-[16px]">project@fragile.studio</h3>
+                      <h2 className="text-[12px] uppercase mt-[60px] mb-2">
+                        FOR INTERNSHIP
+                      </h2>
+                      <h3 className="text-[16px]">coffee@fragile.studio</h3>
+                      <h2 className="text-[12px] uppercase mt-[60px] mb-2">
+                        FOR HIGH FIVE
+                      </h2>
+                      <h3 className="text-[16px]">charles@fragile.studio</h3>
+                      <h3 className="text-[16px]">david@fragile.studio</h3>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,14 @@
 import Logo from "assets/logo.svg";
 import Hand from "assets/hand.svg";
 
-export const Header = () => {
+export const Header = ({ handleDrawer }: { handleDrawer: () => void }) => {
   return (
     <div>
       <div className="bg-white w-full md:h-[45px] h-[20px] z-10 relative "></div>
       <div className="lg:px-[60px] px-[25px] flex justify-between w-full">
         <Logo />
         <div className="-mt-[60px] z-0 hover:-mt-[0px] hover:-mb-[60px] cursor-pointer transition-all ease-in-out ">
-          <Hand />
+          <Hand onClick={handleDrawer} />
         </div>
       </div>
       <div className="w-full lg:mt-[150px] mt-[90px]  lg:text-[64px] text-[40px] lg:ml-60 px-[25px] font-fragile ">
