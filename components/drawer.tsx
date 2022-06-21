b8d1f2f4cfa6614f16d416cb7c49906317be383c
charlesdoiron <doironcharles@gmail.com>
2022-06-21 20:41:23 +0200

    btn outline

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -37,7 +37,7 @@ export const Drawer = ({
                       <div className="flex items-start justify-end">
                         <div className="ml-3 flex h-7 items-center">
                           <button type="button" onClick={handleDrawer}>
-                            <span className="underline text-white">
+                            <span className="underline text-white outline-0">
                               No thanks !
                             </span>
                           </button>
