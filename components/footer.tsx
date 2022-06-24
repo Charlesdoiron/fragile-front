b8611e8768bf4fac38e7b322fef14dfb91e1a603
charlesdoiron <doironcharles@gmail.com>
2022-06-24 16:43:50 +0200

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="md:mt-40 mt-20 text-[#FF5C00] border-t   w-full md:py-20 py-5 px-[25px] md:px-8  text-right text-[12px] flex justify-between md:relative bottom-0 absolute">
+    <div className="md:mt-40 mt-20 text-[#FF5C00] border-t   w-full md:py-20 py-5 px-[25px] md:px-8  text-right text-[12px] flex justify-between relative ">
       <p>Studio Fragile - 2022</p>
       <p> design + code for precious projects</p>
     </div>
