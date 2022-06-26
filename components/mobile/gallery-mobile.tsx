5650756aadda975efc2180b4b447a7db330c5b21
charlesdoiron <doironcharles@gmail.com>
2022-06-26 16:11:24 +0200

    mobile

--- components/mobile/gallery-mobile.tsx
+++ components/mobile/gallery-mobile.tsx
@@ -6,19 +6,34 @@ export const GalleryMobile = ({ isOpen = false }: { isOpen: boolean }) => {
   return (
     <div
       className={`bg-black p-0  w-full flex   ${
-        isOpen ? "max-h-[600px] duration-500" : "max-h-0  duration-500"
+        isOpen ? "max-h-[600px] duration-500" : "max-h-0  duration-500 none"
       }`}
     >
       <div
-        className={` flex ${
+        className={` flex snap-x mx-auto snap-mandatory   w-screen overflow-scroll ${
           isOpen
-            ? "opacity-1 duration-[800ms] translate-y-0 ease-in-out"
-            : "opacity-0 duration-[100ms] -translate-y-1  ease-in-out"
+            ? "opacity-1 duration-[800ms] translate-y-0 ease-in-out h-[250px]"
+            : "opacity-0 duration-[100ms] -translate-y-1  ease-in-out "
         }  `}
       >
-        <Image src={Mock} alt="mock" />
-
-        <Image src={Mock2} alt="mock" className="ml-10 relative left-2" />
+        <div className="snap-start  flex-shrink-0 w-[90%]   flex items-center justify-center  mr-3">
+          <Image src={Mock} alt="plaf" />
+        </div>
+        <div className="snap-start flex-shrink-0  w-[90%]  flex items-center justify-center mr-3">
+          <Image src={Mock2} alt="plaf" />
+        </div>
+        <div className="snap-start  flex-shrink-0 w-[90%] flex items-center justify-center mr-3">
+          <Image src={Mock} alt="plaf" />
+        </div>
+        <div className="snap-start flex-shrink-0  w-[90%] flex items-center justify-center mr-3">
+          <Image src={Mock2} alt="plaf" />
+        </div>
+        <div className="snap-start  flex-shrink-0 w-[90%] flex items-center justify-center mr-3">
+          <Image src={Mock} alt="plaf" />
+        </div>
+        <div className="snap-start  flex-shrink-0 w-[90%] flex items-center justify-center mr-3">
+          <Image src={Mock2} alt="plaf" />
+        </div>
       </div>
     </div>
   );
